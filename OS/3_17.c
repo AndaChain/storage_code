e04fbd99b3fece4880ca2df646e2210932ada3ab
AndaChain <32815766+AndaChain@users.noreply.github.com>
2021-08-31 22:50:50 +0700

    Create 3_17.c

---
+++ OS/3_17.c
@@ -0,0 +1,32 @@
+/*
+ * REF: https://www.alltestanswers.com/operating-systems-fundamentals-assignment-2/
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+pid_t pid;
+	
+	pid = fork();
+	
+	if (pid == 0)
+	{
+		printf("CHILD pid: %d\n", getpid());
+		exit(0);
+	}
+	else if (pid > 0)
+	{
+		printf("PARENT pid: %d\n", getpid());
+		sleep(10);
+	}
+	else
+	{
+		printf("Failed to fork!\n");
+		return -1;
+	}
+
+	return 0;
+	
+}
